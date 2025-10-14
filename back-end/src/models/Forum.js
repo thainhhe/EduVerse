c297875aa9e1939329e6e812d4ed0ec5fb946f4f
HuyTQ <huytqhe170798@fpt.edu.vn>
2025-10-14 21:42:07 +0700

    update model Score User Comment Forum Log Permission

---
+++ back-end/src/models/Forum.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const forumSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String },
+        courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
+        createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+    },
+    {
+        timestamps: true,
+        collection: "forums",
+    }
+);
+module.exports = mongoose.model("Forum", forumSchema);
