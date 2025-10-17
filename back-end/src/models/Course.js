a0499f7672f3be03cc80fd234f7244aadaaf76c2
HuyTQ <huytqhe170798@fpt.edu.vn>
2025-10-17 21:45:07 +0700

    update model ver 1.0.0

--- back-end/src/models/Course.js
+++ back-end/src/models/Course.js
@@ -4,13 +4,18 @@ const courseSchema = new mongoose.Schema(
     {
         title: { type: String, required: true },
         description: { type: String },
-        instructor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        main_instructor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        instructors: [
+            {
+                id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+                permission: [{ type: mongoose.Schema.Types.ObjectId, ref: "Permission" }],
+            },
+        ],
         category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
         isPublished: { type: Boolean, default: false },
         price: { type: Number, default: 0, min: 0 },
         rating: { type: Number, default: 0, min: 0, max: 5 },
         totalEnrollments: { type: Number, default: 0, min: 0 },
-        tags: { type: [String], default: [] },
         status: { type: String, enum: ["draft", "published", "archived"], default: "draft" },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now },
--- back-end/src/models/Lesson.js
+++ back-end/src/models/Lesson.js
@@ -8,6 +8,7 @@ const lessonSchema = new mongoose.Schema(
         type: { type: String, enum: ["video", "article", "quiz"], default: "article" },
         duration: { type: Number, default: 0, min: 0 },
         order: { type: Number, required: true },
+        user_completed: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
         resources: { type: [String], default: [] },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now },
